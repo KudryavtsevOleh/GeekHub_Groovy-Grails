c2e69e662537064dba5ae5f53e679dd9cb5ddb73
Kudryavtsev_O <maxalonso3@gmail.com>
2015-11-23 12:06:50 +0200

    Change objects fields.

--- Lesson5/src/com/oleh/groovy/Main.groovy
+++ Lesson5/src/com/oleh/groovy/Main.groovy
@@ -14,8 +14,8 @@ class Main {
 
     static {
         ducks << new DuckWithDef(12, 23, 12.67, 1.5)
-        ducks << new DuckWithDef(Color.BLACK_AND_WHITE, DuckBreed, 12.67, 1.5)
-        ducks << new DuckWithDef(12, 23, 12.67, 1.5)
+        ducks << new DuckWithDef(Color.BLACK_AND_WHITE, DuckBreed.AMERICAN_PEKIN_DUCK, true, {val -> val + 3})
+        ducks << new DuckWithDef('duck', Color.BLACK_AND_WHITE, DuckBreed.BAU_DUCK, '1.5')
         ducks << new DuckWithoutDef(Color.WHITE, DuckBreed.ABACOT_RANGER, 1.45, 12.45)
     }
 
